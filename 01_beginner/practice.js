91f8bdba0bc62e082a71e9599410c3fb00f7dc0e
marufxdx <marufxdx444@gmail.com>
2025-02-07 07:25:55 -0800

    conversion of data

--- 01_beginner/practice.js
+++ 01_beginner/practice.js
@@ -1,3 +1,4 @@
+
 /* variable */
 
 const pi = 3.14; // const value can't be changed
@@ -5,6 +6,8 @@ let maruf = 10; // let value can be changed
 var hasan = 20;  // var has global scope which is not good practice.
 user = "maruf" // without let, var, const it will be global variable.
 console.table({pi, maruf, hasan , user}); 
+
+
 
 
 /* data types */
@@ -17,7 +20,35 @@ console.table({pi, maruf, hasan , user});
 // undefined => 
 // symbol => unique
 // object
-
+// array
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+
+
+
+
+/* conversion */
+
+// string to number
+
+let score = "10";
+console.log(typeof Number(score)); // string to number
+let score_1 = "10a";
+console.log(Number(score_1)); // NaN => Not a Number but type is sill number
+
+// "10" => 10
+// "10a" => NaN
+//  true => 1; false => 0
+//  null => 0
+
+
+//boolean to number
+
+let isTrue = 0;
+console.log(Boolean(isTrue));
+
+// 0 => false; 1 => true
+// "" => false; "Maruf" => true
+// undefined => false; null => false
 
