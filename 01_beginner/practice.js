95d051da33a4842c1780a11f270b253f5edc1791
marufxdx <marufxdx444@gmail.com>
2025-02-07 03:33:36 -0800

    variable and datatype

--- 01_beginner/practice.js
+++ 01_beginner/practice.js
@@ -1,1 +1,23 @@
-console.log("Maruf Hasan");
+/* variable */
+
+const pi = 3.14; // const value can't be changed
+let maruf = 10; // let value can be changed
+var hasan = 20;  // var has global scope which is not good practice.
+user = "maruf" // without let, var, const it will be global variable.
+console.table({pi, maruf, hasan , user}); 
+
+
+/* data types */
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
