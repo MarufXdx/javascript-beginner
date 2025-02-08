329414cfb48fe735f0335ae61494c7e1c8afd5f4
marufxdx <marufxdx444@gmail.com>
2025-02-08 17:40:21 +0600

    memory

--- 01_beginner/practice.js
+++ 01_beginner/practice.js
@@ -30,6 +30,28 @@ console.log(typeof null); // object
 
 
 
+/* memory type */
+
+// stack(primitive) and heap(reference/non-primitive)
+// stack => copy; heap => reference
+
+// stack
+let Maruf = 10;
+let Hasan = Maruf; // hasan gets the copy value of maruf
+Hasan = 20;
+console.log(Maruf , Hasan); //10 20
+
+// heap
+let user_1 = {
+    name: "maruf",
+    age: 20
+}
+let user_2 = user_1; // user_2 gets the reference value of user_1 so if any of them changes the value, both will be changed.
+user_1.age = 21;
+console.log(user_1.age , user_2.age); //21 21
+
+
+
 /* conversion */
 
 // string to number
